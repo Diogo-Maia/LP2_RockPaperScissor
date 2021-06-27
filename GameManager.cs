83ef328a65b0376cb1ce7a580faea029b0172ca5
Diogo-Maia <Diogo_Maia99@hotmail.com>
2021-06-27 22:42:51 +0100

    Executa os eventos e baralha-os com o FisherYates

--- GameManager.cs
+++ GameManager.cs
@@ -5,11 +5,10 @@ namespace LP2_RockPaperScissor.Common
 {
     public class GameManager
     {
-        private int xdim, ydim;
-        private double swap_rate_exp, repr_rate_exp, selc_rate_exp;
+        private readonly int xdim, ydim;
+        private readonly double swap_rate_exp, repr_rate_exp, selc_rate_exp;
 
-        private Place[,] map;
-        private IView ui;
+        private readonly Place[,] map;
 
         public GameManager(int xdim, int ydim,
             double swap_rate_exp, double repr_rate_exp, double selc_rate_exp)
@@ -25,17 +24,17 @@ namespace LP2_RockPaperScissor.Common
 
         public void Start(IView ui)
         {
-            this.ui = ui;
-
             FillMap();
-            ui.MapView(map, xdim, ydim);
+            GameLoop(ui);
+        }
 
-            //Loop so ca esta para ver o swap a funcionar
+        private void GameLoop(IView ui)
+        {
             while (true)
             {
                 Console.Clear();
-                //map[1, 1].Swap(map, 1, 1, xdim, ydim);
-                map[1, 1].Reproduction(map, 1, 1, xdim, ydim);
+                ExecuteEvents(GenerateEvents());
+
                 ui.MapView(map, xdim, ydim);
                 System.Threading.Thread.Sleep(500);
             }
@@ -86,12 +85,36 @@ namespace LP2_RockPaperScissor.Common
                 events.Add(Events.Selection);
             }
 
-            return events;
+            return FisherYates(events);
         }
 
-        private List<char> FisherYates(List<char> toShuffle)
+        private void ExecuteEvents(List<Events> events)
         {
-            List<char> shuffle = new List<char>();
+            Random rdn = new Random();
+            int r_x;
+            int r_y;
+            foreach (Events e in events)
+            {
+                r_x = rdn.Next(0, xdim - 1);
+                r_y = rdn.Next(0, ydim - 1);
+                switch (e)
+                {
+                    case Events.Swap:
+                        map[r_x, r_y].Swap(map, r_x, r_y, xdim, ydim);
+                        break;
+                    case Events.Reproduction:
+                        map[r_x, r_y].Reproduction(map, r_x, r_y, xdim, ydim);
+                        break;
+                    case Events.Selection:
+                        map[r_x, r_y].Selection(map, r_x, r_y, xdim, ydim);
+                        break;
+                }
+            }
+        }
+
+        private List<Events> FisherYates(List<Events> toShuffle)
+        {
+            List<Events> shuffle = new List<Events>();
             Random rdn = new Random();
             List<int> n = new List<int>();
 
